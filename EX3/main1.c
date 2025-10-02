e1d76b885edd2e6e07513fe9d1060058eb716399
B1429011 <B1429011@cgu.edu.tw>
2025-10-02 14:05:43 +0800

    Create main1.c

---
+++ EX3/main1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void print_sp(int i, int n) {
+    for (int sp = 0; sp < n-i;sp++) {
+        printf(" ");
+    }
+}
+
+void print_num(int i) {
+    for (int j = 0; j < i;j++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+}
+
+int main() {
+    int rows = 6;
+    for (int i =0; i<= rows; i++) {
+        print_sp(i, rows);
+        print_num(i);
+    }
+    return 0;
+}
